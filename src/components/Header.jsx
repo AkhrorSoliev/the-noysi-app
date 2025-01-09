eb3d6d20fbe55e9949139fc053f337358923d8bf
Akhror <akhsoliev@gmail.com>
2025-01-09 22:50:38 +0500

    feat: implement Header component, replace MobileHeader, and enhance theme management

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,13 @@ import { useSignOut } from "../hooks/useSignOut";
 import { IoMdMoon, IoMdSunny } from "react-icons/io";
 import { Link } from "react-router-dom";
 import { useDocument } from "../hooks/useDocument";
+import Logo from "../assets/noysi.svg";
 
 const themeFromLocalStorage = () => {
   return localStorage.getItem("theme") || "light";
 };
 
-function MobileHeader() {
+function Header() {
   const { user } = useGlobalContext();
   const { document: _document } = useDocument("users", user.uid);
   const { signout } = useSignOut();
@@ -44,7 +45,7 @@ function MobileHeader() {
 
   return (
     <header
-      className={`fixed left-1/2 top-0 z-50 flex w-full -translate-x-1/2 items-center justify-between px-5 py-4 transition duration-300 md:hidden ${
+      className={`fixed left-1/2 top-0 z-50 flex w-full -translate-x-1/2 items-center justify-between px-5 py-4 transition duration-300 md:sticky md:mb-10 md:-translate-x-0 md:bg-base-100 md:shadow-md ${
         isScrolled ? "h-[80px] bg-base-100 shadow-md" : "bg-transparent"
       }`}
     >
@@ -52,12 +53,17 @@ function MobileHeader() {
         <div
           tabIndex={0}
           role="button"
-          className="avatar btn btn-circle btn-ghost"
+          className="avatar btn btn-circle btn-ghost md:hidden"
         >
           <div className="w-12 rounded-full ring-2 ring-primary ring-offset-2 ring-offset-base-100">
             <img src={_document?.photoURL} alt={_document?.displayName} />
           </div>
         </div>
+        <img
+          src={Logo}
+          alt="Noysi website logo"
+          className="hidden w-12 rounded-full md:block"
+        />
         <ul
           tabIndex={0}
           className="menu dropdown-content menu-sm z-[1] mt-1 w-52 rounded-box bg-base-100 p-3 shadow"
@@ -79,11 +85,14 @@ function MobileHeader() {
           </li>
         </ul>
       </div>
-      <button className="btn" onClick={signout}>
+      <button
+        className="btn btn-sm border-0 bg-transparent md:btn-md"
+        onClick={signout}
+      >
         <MdOutlineExitToApp className="text-3xl" />
       </button>
     </header>
   );
 }
 
-export default MobileHeader;
+export default Header;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as FormInput } from "./FormInput";
 export { default as MobileTabletNav } from "./MobilTabletNav";
-export { default as MobileHeader } from "./MobileHeader";
+export { default as Header } from "./Header";
 export { default as OnlineUsers } from "./OnlineUsers";
 export { default as Sidebar } from "./Sidebar";
